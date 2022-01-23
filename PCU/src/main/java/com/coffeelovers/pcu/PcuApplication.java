358901a5aed215893e8d27b3762aabc3c0dbce77
Thays Casado <thayscasado@hotmail.com>
2022-01-23 12:07:13 -0800

    Create the project set up with all the 6 frameworks

---
+++ PCU/src/main/java/com/coffeelovers/pcu/PcuApplication.java
@@ -0,0 +1,13 @@
+package com.coffeelovers.pcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcuApplication.class, args);
+	}
+
+}
---
+++ PCU/src/test/java/com/coffeelovers/pcu/PcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeelovers.pcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
