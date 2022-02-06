0676e6a91c9d4475e5c9d77fa378a3cfce6e9a25
Thays Casado <thayscasado@hotmail.com>
2022-02-05 20:22:40 -0800

    Added setters and getters for the product table

--- PCU/src/main/java/com/coffeelovers/pcu/model/Product.java
+++ PCU/src/main/java/com/coffeelovers/pcu/model/Product.java
@@ -16,20 +16,71 @@ public class Product {
 	private long productID;
 	
 	@Column(name= "materialsID")
-	private long MaterialsID;
+	private long materialsID;
 	
 	@Column(name= "variableCosts")
-	private long variableCosts;
+	private double variableCosts;
 	
 	@Column(name= "pic")
 	private long pic;
 	
 	@Column(name= "size")
-	private long size;
+	private String size;
+	
 	public Product() {
 		
 	}
+
+	// Personalized
+	public Product(long materialsID, double variableCosts, long pic, String size) {
+		this.materialsID = materialsID;
+		this.variableCosts = variableCosts;
+		this.pic = pic;
+		this.size = size;
+	}
 	
-	// Generate Setters and getters
+	
+// Generate Setters and getters
+
+	public long getProductID() {
+		return productID;
+	}
+
+	public void setProductID(long productID) {
+		this.productID = productID;
+	}
+
+	public long getMaterialsID() {
+		return materialsID;
+	}
+
+	public void setMaterialsID(long materialsID) {
+		this.materialsID = materialsID;
+	}
+
+	public double getVariableCosts() {
+		return variableCosts;
+	}
+
+	public void setVariableCosts(double variableCosts) {
+		this.variableCosts = variableCosts;
+	}
+
+	public long getPic() {
+		return pic;
+	}
+
+	public void setPic(long pic) {
+		this.pic = pic;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
 	
 }
