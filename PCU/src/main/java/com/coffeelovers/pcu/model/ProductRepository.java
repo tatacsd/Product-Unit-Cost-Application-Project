9cf858c4c93c8949b14610c3d4ce3be3ff33fb9b
Thays Casado <thayscasado@hotmail.com>
2022-02-06 11:40:37 -0800

    Update PCU/src/main/java/com/coffeelovers/pcu/model/ProductRepository.java

--- PCU/src/main/java/com/coffeelovers/pcu/model/ProductRepository.java
+++ PCU/src/main/java/com/coffeelovers/pcu/model/ProductRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	// Add the abstract methods
 	List<Product> findByMaterialsID(Long materialID);
 	List<Product> findByvariableCosts(Long variableCosts);
 	List<Product> findBypic(Long pic);
