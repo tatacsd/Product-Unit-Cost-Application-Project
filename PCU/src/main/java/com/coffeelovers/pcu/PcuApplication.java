815abb820ef5d351b2d64fa7473a56ad5bccd3aa
SoAngryGiraffe <koshevenko13@gmail.com>
2022-02-05 17:38:54 -0800

    Deleted comments

--- PCU/src/main/java/com/coffeelovers/pcu/PcuApplication.java
+++ PCU/src/main/java/com/coffeelovers/pcu/PcuApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PcuApplication {
-//TESTING GIT
+
 	public static void main(String[] args) {
 		SpringApplication.run(PcuApplication.class, args);
-		//anything test
+		
 	}
 
 }
