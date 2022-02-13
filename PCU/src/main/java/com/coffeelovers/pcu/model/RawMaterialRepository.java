6cda01c0ec0d0c9ff5c52c5dda492012d77267a5
Thays Casado <thayscasado@hotmail.com>
2022-02-12 19:38:14 -0800

    typo

--- PCU/src/main/java/com/coffeelovers/pcu/model/RawMaterialRepository.java
+++ PCU/src/main/java/com/coffeelovers/pcu/model/RawMaterialRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RawMaterialRepository extends JpaRepository<RawMaterial,Long> {
-	List<RawMaterial> findByMaterialID (long materialID);
+	List<RawMaterial> findByRawMaterialID (long rawMaterialID);
 	List<RawMaterial> findByInvoiceNumber (long invoiceNumber);
 	List<RawMaterial> findBySupplierID (long supplierID);
 }
