ca1f8d05e5c1cf9c400e3ea12c5bd3b42ae89f15
Andrew-Shanan <andrew.shanan@hotmail.com>
2022-02-27 16:59:38 -0800

    invoice is done

---
+++ PCU/src/main/java/com/coffeelovers/pcu/model/Invoice.java
@@ -0,0 +1,100 @@
+package com.coffeelovers.pcu.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="invoices")
+public class Invoice {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "invoiceNumber")
+	private String invoiceNumber;
+	
+	@Column(name = "supplier")
+	private long supplierID;
+	
+	@Column(name = "invoiceValue")
+	private double invoiceValue;
+	
+	@Column(name = "invoiceDate")
+	private String invoiceDate;
+
+	public Invoice() {
+		
+	}
+	
+	
+	public Invoice(String invoiceNumber, long supplierID, double invoiceValue, String invoiceDate) {
+		this.invoiceNumber = invoiceNumber;
+		this.supplierID = supplierID;
+		this.invoiceValue = invoiceValue;
+		this.invoiceDate = invoiceDate;
+	}
+
+	
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getInvoiceNumber() {
+		return invoiceNumber;
+	}
+
+
+	public void setInvoiceNumber(String invoiceNumber) {
+		this.invoiceNumber = invoiceNumber;
+	}
+
+
+	public long getSupplierID() {
+		return supplierID;
+	}
+
+
+	public void setSupplierID(long supplierID) {
+		this.supplierID = supplierID;
+	}
+
+
+	public double getInvoiceValue() {
+		return invoiceValue;
+	}
+
+
+	public void setInvoiceValue(double invoiceValue) {
+		this.invoiceValue = invoiceValue;
+	}
+
+
+	public String getInvoiceDate() {
+		return invoiceDate;
+	}
+
+
+	public void setInvoiceDate(String invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Invoice [id=" + id + ", invoiceNumber=" + invoiceNumber + ", supplierID=" + supplierID
+				+ ", invoiceValue=" + invoiceValue + ", invoiceDate=" + invoiceDate + "]";
+	}
+	
+	
+	
+
+	
+}
---
+++ PCU/src/main/java/com/coffeelovers/pcu/model/InvoiceRepository.java
@@ -0,0 +1,7 @@
+package com.coffeelovers.pcu.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+}
