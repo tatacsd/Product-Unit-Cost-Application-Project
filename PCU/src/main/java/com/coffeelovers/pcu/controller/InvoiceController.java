7d4a8417ea0109724ad9ba09c58c3687aaf915f5
Thays Casado <thayscasado@hotmail.com>
2022-03-13 13:59:35 -0700

    invoice get one

--- PCU/src/main/java/com/coffeelovers/pcu/controller/InvoiceController.java
+++ PCU/src/main/java/com/coffeelovers/pcu/controller/InvoiceController.java
@@ -1,10 +1,17 @@
 package com.coffeelovers.pcu.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coffeelovers.pcu.model.Invoice;
 import com.coffeelovers.pcu.model.InvoiceRepository;
 
 
@@ -16,25 +23,13 @@ public class InvoiceController {
 	@Autowired
 	InvoiceRepository invoiceRepository;
 
-	// @GetMapping("/fixedCosts")
-	// public ResponseEntity<List<FixedCost>> getAllFixedCosts(){	
-	// 	try {			
-	// 		List<FixedCost> fixedCostsArray = new ArrayList<FixedCost>();	
-	// 		fixedCostRepository.findAll().forEach(fixedCostsArray::add);
-	// 		return new ResponseEntity<>(fixedCostsArray,HttpStatus.OK);
-
-	// 	}catch (Exception e) {
-	// 		return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
-	// 	}		
-	// }
-
 	// Get one
-	@GetMapping("/invoiceController/{id}")
-	public ResponseEntity<InvoiceRepository> getFixedCostsById(@PathVariable("id") long fixedCostsID) {
+	@GetMapping("/invoice/{id}")
+	public ResponseEntity<Invoice> getFixedCostsById(@PathVariable("id") long invoiceID) {
 		try {
-			Optional<InvoiceRepository> fixedCostsData = invoiceRepository.findById(fixedCostsID);
-			if (fixedCostsData.isPresent())
-				return new ResponseEntity<>(fixedCostsData.get(), HttpStatus.OK);
+			Optional<Invoice> invoiceData = invoiceRepository.findById(invoiceID);
+			if (invoiceData.isPresent())
+				return new ResponseEntity<>(invoiceData.get(), HttpStatus.OK);
 			else
 				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		} catch (Exception e) {
