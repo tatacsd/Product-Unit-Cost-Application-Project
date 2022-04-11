4798bca950b55680598c3edef289e29d2700b8b2
andrew <andrew.shanan@hotmail.com>
2022-04-11 02:04:38 -0700

    The report is alomst done. please check

--- backend/src/main/java/com/coffeelovers/pcu/controller/ReportController.java
+++ backend/src/main/java/com/coffeelovers/pcu/controller/ReportController.java
@@ -1,7 +1,12 @@
 package com.coffeelovers.pcu.controller;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,27 +31,74 @@ public class ReportController {
 	SupplierRepository supplierRepository;
 	
 	/**
-	 * this method retern the filal report
+	 * this method return the final report
 	 * @return
 	 */
 	@GetMapping("/report")
 	public ResponseEntity<Object> getAllProduct(){
-		List<Object> report = new ArrayList<>();
+		Dictionary<String,Object> reportArray = new Hashtable<>();
+		long productCount; 
+		OptionalDouble productMaxValue; 
+		OptionalDouble productMinValue;
+		String productCostStandardDeviation ;
+		OptionalDouble rawMaterialMaxValue; 
+		OptionalDouble varaiableMaxValue; 
+
 		
 		try {
-			report.add(productRepository.findAll().stream().count()); // number of product
-			// most expensive product
-			// most cheapest product
-			
-			// most expensive product in term of raw materials
-			// most expensive product in term of variable cost
+			// product part
+				productCount = productRepository.findAll().stream().count();
+				reportArray.put("number of products",productCount); // number of products
+				reportArray.put("Avarage cost of products",productRepository.findAll().stream().mapToDouble(productValue -> productValue.getNetCost()).average());
+				//
+				productCostStandardDeviation = calculateSD(productRepository.findAll().stream().mapToDouble(productValue -> productValue.getNetCost()).sum(),
+						productRepository.findAll().stream().map(v->v.getNetCost()).toArray());
 
-			report.add(supplierRepository.findAll().stream().count()); // number of suppliers
+				
+				reportArray.put("Product Cost Standard Deviation",productCostStandardDeviation);
+
+				productMaxValue = productRepository.findAll().stream().mapToDouble(productValue -> productValue.getNetCost()).max(); // the most expensive product
+				reportArray.put("Most Expensive Product in term of Net value",productMaxValue);
+				reportArray.put("Most Expensive Product in term of Net value-Code",productRepository.findAll().stream().filter(productValue ->productValue.getNetCost()== productMaxValue.getAsDouble()).findFirst().map(c->c.getCode()));// most  expensive  product's code
+				//
+				productMinValue = productRepository.findAll().stream().mapToDouble(productValue -> productValue.getNetCost()).min(); // the cheapest product
+				reportArray.put("cheapest Product term of Net value",productMinValue);
+				reportArray.put("cheapest Product term of Net value-Code",productRepository.findAll().stream().filter(productValue ->productValue.getNetCost()== productMinValue.getAsDouble()).findFirst().map(c->c.getCode()));// most  cheapest  product's code
+				
+				// most expensive product in term of raw materials
+				rawMaterialMaxValue = productRepository.findAll().stream().mapToDouble(productValue -> productValue.getTotalMaterialCost()).min(); 
+				reportArray.put("most expensive product in term of raw materials",rawMaterialMaxValue);
+				reportArray.put("most expensive product in term of raw materials code",productRepository.findAll().stream().filter(productValue ->productValue.getTotalMaterialCost()== rawMaterialMaxValue.getAsDouble()).findFirst().map(c->c.getCode()));// most  cheapest  product's code
+				// most cheapest product in term of variable cost
+				varaiableMaxValue = productRepository.findAll().stream().mapToDouble(productValue -> productValue.getTotalvariableCost()).min(); 
+				reportArray.put("The Cheapest product in term of Varaible Cost ",varaiableMaxValue);
+				reportArray.put("The Cheapest product in term of Varaible Cost-Code ",productRepository.findAll().stream().filter(productValue ->productValue.getTotalvariableCost()== varaiableMaxValue.getAsDouble()).findFirst().map(c->c.getCode()));// most  cheapest  product's code
+				
+				// 
+			// supplier's part
+				reportArray.put("Number of Suppliers",supplierRepository.findAll().stream().count()); // number of suppliers
+				
 			
-			return new ResponseEntity<>(report, HttpStatus.OK);
+			return new ResponseEntity<>(reportArray, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
+	
+	// this mothod calc the Standard Deviation of the product cost  
+	public static String calculateSD(Double sum, Object[] costArray)
+    {
+		DecimalFormat df = new DecimalFormat("0.00");
+				
+		double standardDeviation = 0.0;
+       
+       double mean = sum/costArray.length;
+
+        for(Object num: costArray) 
+            standardDeviation += Math.pow((double)num - mean, 2);
+        	
+
+        return df.format(Math.sqrt(standardDeviation/costArray.length)) ;
+    } 
 }
